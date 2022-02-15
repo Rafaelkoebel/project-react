d026052d5a817d0bdde1a2b8cab5b9bedec40d1a
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-02-15 18:52:46 +0100

    TOP

---
+++ projet-chart/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BarChart from './charts/BarChart';
+
+function App() {
+  return (
+    <div className="App">
+      <BarChart />
+    </div>
+  );
+}
+
+export default App;
