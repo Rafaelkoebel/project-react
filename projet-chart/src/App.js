f8e35673cf7d2a6b475321eb77079257a13b4203
Rafaelkoebel <rafael.koebel@gmail.com>
2022-02-16 10:01:44 +0100

    top

--- projet-chart/src/App.js
+++ projet-chart/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import './App.css'
 import BarChart from './charts/BarChart'
+import DoughnutChart from './charts/DoughnutChart'
+import LineChart from './charts/LineChart'
+import PieChart from './charts/PieChart';
 
 function App() {
   return (
     <div className="App">
        <BarChart /> 
+       <DoughnutChart/>
+       <LineChart />
+       <PieChart />
     </div>
   );
 }
--- projet-chart/src/charts/BarChart.js
+++ projet-chart/src/charts/BarChart.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Legend,  } from 'chart.js'
 import { Bar } from 'react-chartjs-2'
 
@@ -11,40 +11,11 @@ ChartJS.register(
 
 const BarChart = () => {
 
-    const [chart, setChart] = useState([])
-
-    var baseUrl = "https://api.coinranking.com/v2/coins/?limit=10"
-    var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    var apiKey = 
-    "coinranking70234f353495f0fab29a2d6362759f74861c7223356111fd"
-
-    useEffect(() => {
-        const fetchCoins = async () => {
-            await fetch (`${proxyUrl}${baseUrl}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-access-token': `${apiKey}`,
-                    'Access-Control-Allow-Origin': '*'
-                }
-            }).then((response) => {
-                response.json().then((json) =>{
-                    console.log(json)
-                    setChart(json.data)
-                })
-            }).catch(error =>{
-                console.log(error);
-            })
-        }
-        fetchCoins()
-    }, [])
-
-    
     const data= {
-        labels: chart?.coins?.map(x => x.name),
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
             label: 'Mon premier test' ,
-            data: chart?.coins?.map(x => x.price),
+            data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
